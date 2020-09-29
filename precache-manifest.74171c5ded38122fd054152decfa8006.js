a5102b1cfff2a8ae232c25a0e48e34881d8559d9
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-29 13:26:18 +0100

    Updates

---
+++ precache-manifest.74171c5ded38122fd054152decfa8006.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82258c6aacc56813337bb2a693065de",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "e8771e2fe94151c07628",
+    "url": "/portfolio-website/static/css/main.471c9512.chunk.css"
+  },
+  {
+    "revision": "8aed8b6a30bcc709815d",
+    "url": "/portfolio-website/static/js/2.84927b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-website/static/js/2.84927b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8771e2fe94151c07628",
+    "url": "/portfolio-website/static/js/main.9eea95f7.chunk.js"
+  },
+  {
+    "revision": "56629c2812097c1c0d9b",
+    "url": "/portfolio-website/static/js/runtime-main.e65ae6a1.js"
+  },
+  {
+    "revision": "68d1263f684eabf222dff4864c378f61",
+    "url": "/portfolio-website/static/media/Austin-Booth-thumb-180x180.68d1263f.png"
+  },
+  {
+    "revision": "51e154fa1b4dc9547800eb434af5f098",
+    "url": "/portfolio-website/static/media/nc-news-be.51e154fa.png"
+  },
+  {
+    "revision": "9029959e72dae2b45bbbc1aa23b04121",
+    "url": "/portfolio-website/static/media/nc-news-fe.9029959e.png"
+  }
+]);
