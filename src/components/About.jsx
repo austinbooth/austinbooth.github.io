8581b2cc4c1794dfb34e58207f04a0bb48d089eb
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-29 16:07:33 +0100

    adds links and tweaks text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,11 +7,11 @@ const About = () => {
       <img src={me} alt="A picture of Austin Booth" />
       <h2>I'm Austin, a software developer</h2>
       <p>
-        Originally I studied chemistry and worked in the pharmaceutical and
-        health and safety industries. Following this I worked as a science
-        teacher for 10 years. However for the last few years of my teaching
-        career I felt bored and unfulfilled. I wasn't being challenged or
-        learning anything new.
+        At University I studied chemistry and subsequently worked in the
+        pharmaceutical and health and safety industries. Following this, I
+        worked as a science teacher for 10 years. However for the last few years
+        of my teaching career I felt bored and unfulfilled. I wasn't being
+        challenged or learning anything new.
       </p>
       <p>
         Since being young I'd always tinkered with computer programming. I had a
@@ -19,24 +19,31 @@ const About = () => {
         However this is as far as my programming went at the time.
       </p>
       <p>
-        Much later I had a go at programming a PHP website that would generate
-        questions for my classes. This sparked my interest in programming again
-        and I loved that I'd made something that was useful.
+        Much later, whilst working as a teacher, I had a go at programming a PHP
+        website that would generate questions for my classes. This sparked my
+        interest in programming again and I loved that I'd made something that
+        was useful.
       </p>
       <p>
-        So I decided to pursue programming much more. I'd also learnt a little
-        about data science and Python and so decided to learn some basics with
-        Dataquest. After learning how to use Python to do some basic data
-        analysis and machine learning I was hooked! I always loved solving
-        problems, and this was exactly what I was doing with programming.
+        So I decided to pursue programming much more. I'd heard a lot about data
+        science and Python and so decided to learn some basics with{" "}
+        <a href="https://www.dataquest.io/" target="_blank">
+          Dataquest
+        </a>
+        . After learning how to use Python to do some basic data analysis and
+        machine learning I was hooked! I'd always loved solving problems, and
+        this was exactly what I was doing with programming.
       </p>
       <p>
         And so I decided that I wanted to change career and be a software
         developer. However changing career in your early 40s isn't easy. I
         decided that the quickest way would be to enrol in a coding bootcamp.
-        After researching several, the one that really stood out was
-        Northcoders. I decided to apply and successfully completed the tech
-        test! This was April 2020.
+        After researching several, the one that really stood out was{" "}
+        <a href="https://northcoders.com/" target="_blank">
+          Northcoders
+        </a>
+        . I decided to apply and successfully completed the tech test! This was
+        April 2020.
       </p>
       <p>
         However the Coronavirus pandemic had just started, and all future
