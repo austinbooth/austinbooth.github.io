020ff1fecebd444da977a948514fec50e5805a89
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-30 08:07:58 +0100

    adds data for python udacity projects

--- src/projectData.js
+++ src/projectData.js
@@ -2,46 +2,6 @@ import ncNewsBe from "./images/nc-news-be.png";
 import ncNewsFe from "./images/nc-news-fe.png";
 const projectData = [
   {
-    id: "nc-news-api",
-    title: "NC News API",
-    language: "JavaScript",
-    technologiesShort: ["NodeJS", "Jest (TDD)"],
-    technologiesFull: ["NodeJS", "Express", "Knex", "Postgres", "Jest (TDD)"],
-    date: "July 2020",
-    cardBody:
-      "A RESTful API serving as the backend for a Reddit-style news app.",
-    githubUrl: [
-      {
-        url: "https://github.com/austinbooth/nc-news-backend",
-        desc: null,
-      },
-    ],
-    liveUrl: [{ url: "https://nc-news-ab.herokuapp.com/api/", desc: "" }],
-    image: ncNewsBe,
-    altImage: "alt text",
-    fullBody:
-      "A RESTful API serving as the backend for a Reddit-style news app. Built using TDD with Jest.",
-  },
-  {
-    id: "nc-news",
-    title: "NC News",
-    language: "JavaScript",
-    technologiesShort: ["React"],
-    technologiesFull: ["React", "Axios"],
-    date: "August 2020",
-    cardBody:
-      "A Reddit-style news app which uses my NC News API as the backend.",
-    githubUrl: [
-      {
-        url: "https://github.com/austinbooth/nc-news-backend",
-        desc: null,
-      },
-    ],
-    liveUrl: [{ url: "https://nc-news-ab.netlify.app/", desc: "" }],
-    image: ncNewsFe,
-    altImage: "alt text",
-  },
-  {
     id: "travel-buddy",
     title: "Travel Buddy",
     language: "JavaScript",
@@ -71,6 +31,151 @@ const projectData = [
     image: ncNewsBe,
     altImage: "alt text",
   },
+  {
+    id: "nc-news",
+    title: "NC News",
+    language: "JavaScript",
+    technologiesShort: ["React"],
+    technologiesFull: ["React", "Axios"],
+    date: "August 2020",
+    cardBody:
+      "A Reddit-style news app which uses my NC News API as the backend.",
+    githubUrl: [
+      {
+        url: "https://github.com/austinbooth/nc-news-backend",
+        desc: null,
+      },
+    ],
+    liveUrl: [{ url: "https://nc-news-ab.netlify.app/", desc: "" }],
+    image: ncNewsFe,
+    altImage: "alt text",
+  },
+  {
+    id: "nc-news-api",
+    title: "NC News API",
+    language: "JavaScript",
+    technologiesShort: ["NodeJS", "Jest (TDD)"],
+    technologiesFull: ["NodeJS", "Express", "Knex", "Postgres", "Jest (TDD)"],
+    date: "July 2020",
+    cardBody:
+      "A RESTful API serving as the backend for a Reddit-style news app.",
+    githubUrl: [
+      {
+        url: "https://github.com/austinbooth/nc-news-backend",
+        desc: null,
+      },
+    ],
+    liveUrl: [{ url: "https://nc-news-ab.herokuapp.com/api/", desc: "" }],
+    image: ncNewsBe,
+    altImage: "alt text",
+    fullBody:
+      "A RESTful API serving as the backend for a Reddit-style news app. Built using TDD with Jest.",
+  },
+  {
+    id: "fsnd-deploying-a-simple-flask-api-to-aws",
+    title: "Deploying a Flask API to AWS (Udacity project)",
+    language: "Python",
+    technologiesShort: ["Kubernetes", "AWS", "CI/CD"],
+    technologiesFull: [
+      "Gunicorn",
+      "Docker",
+      "Kubernetes",
+      "AWS EKS",
+      "CI/CD",
+      "CodePipeline",
+      "CodeBuild",
+      "Flask",
+    ],
+    date: "May 2020",
+    cardBody:
+      "Deployment of a simple Flask API to a Kubernetes cluster using Docker, AWS EKS, CodePipeline and CodeBuild.",
+    githubUrl: [
+      {
+        url:
+          "https://github.com/austinbooth/FSND-Deploy-Flask-App-to-Kubernetes-Using-EKS",
+        desc: null,
+      },
+    ],
+    liveUrl: [{ url: null, desc: null }],
+    image: ncNewsBe,
+    altImage: "alt text",
+  },
+  {
+    id: "fsnd-coffee-shop",
+    title: "Coffee Shop (Udacity project)",
+    language: "Python",
+    technologiesShort: ["Flask", "Auth0"],
+    // technologiesFull: ["blah"],
+    date: "May 2020",
+    cardBody:
+      "I completed an already existing codebase by implementing Auth0 JWT authentication for a ficticious coffee shop website.",
+    githubUrl: [
+      {
+        url:
+          "https://github.com/austinbooth/FSND-project-3-coffee-shop/tree/master/backend",
+        desc: null,
+      },
+    ],
+    liveUrl: [{ url: null, desc: null }],
+    image: ncNewsBe,
+    altImage: "alt text",
+  },
+  {
+    id: "fsnd-trivia-app",
+    title: "Trivia app (Udacity project)",
+    language: "Python",
+    technologiesShort: ["Flask", "SQLAlchemy", "PostgreSQL"],
+    // technologiesFull: ["blah"],
+    date: "May 2020",
+    cardBody:
+      "I completed an already exisiting codebase by adding several API endpoints to enable CRUD operations on quiz questions stored in the PostgreSQL database.",
+    githubUrl: [
+      {
+        url:
+          "https://github.com/austinbooth/FSND-project-2-Trivia-app/tree/master/backend",
+        desc: null,
+      },
+    ],
+    liveUrl: [{ url: null, desc: null }],
+    image: ncNewsBe,
+    altImage: "alt text",
+  },
+  {
+    id: "fsnd-fyyur",
+    title: "Fyyur (Udacity project)",
+    language: "Python",
+    technologiesShort: ["Flask", "SQLAlchemy", "PostgreSQL"],
+    // technologiesFull: ["tech"],
+    date: "May 2020",
+    cardBody: "I completed an already exisiting codebase by adding models to allow the app to interact with a PostgreSQL database.",
+    githubUrl: [
+      {
+        url: "https://github.com/austinbooth/FSND-project-1-Fyyur",
+        desc: null,
+      },
+    ],
+    liveUrl: [{ url: null, desc: null }],
+    image: ncNewsBe,
+    altImage: "alt text",
+  },
+  // {
+  //   id: "blank",
+  //   title: "blank",
+  //   language: "Python",
+  //   technologiesShort: ["tech"],
+  //   technologiesFull: ["tech"],
+  //   date: "May 2020",
+  //   cardBody: "body",
+  //   githubUrl: [
+  //     {
+  //       url: "url",
+  //       desc: null,
+  //     },
+  //   ],
+  //   liveUrl: [{ url: null, desc: null }],
+  //   image: ncNewsBe,
+  //   altImage: "alt text",
+  // },
 ];
 
 export default projectData;
